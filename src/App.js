6164b775665af64d26b9c316b268f69de7e07db6
v1ll4n <v1ll4n@villanch.top>
2019-01-02 19:04:05 +0800

    create a router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+
+import {BrowserRouter as Router, Link, Route} from "react-router-dom";
+
+function testRoute() {
+    return (
+        <div>
+            This is Router Content
+        </div>
+    )
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <Link to="/test">Test</Link>
+                    <Route path="/test" component={testRoute}/>
+                </div>
+            </Router>
+
+        );
+    }
 }
 
 export default App;
